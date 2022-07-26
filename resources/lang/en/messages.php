fa33a0a40c53c603c86ff0029a0fd110da82b989
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2022-07-26 18:58:52 +0530

    updated

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'success' => 'Success',
+    'failed' => 'Failed',
+    'validation_error' => 'Validation Errors',
+    'user_email_not_available' => 'Your Email Id not available. Please enter the another Email Id.',
+    'user_email_unverify' => 'Your Email Id is Unverify. Please Verify email and sign in us.',
+    'user_passowrd_wrong' => 'Your password is wrong. Please check and try again.',
+    'not_found' => 'No Data Found',
+    'endtime_greater_starttime' => "End date can't be before Start date",
+    "beetween_startendtime" => "Break time between start time and end time",
+    "wrongmail" => "Your Email Id Is wrong. Please check and enter another email",
+    "busytime_check_appointment" => "The staff is busy for the time being. Please change the time or staff for appointment booking.",
+    "busytime_check_appointment2" => "This time the staff appointment is already booked. For busy times please change the time or staff.",
+    "otp_send" => "OTP is sent to Your Email",
+    "error_PaymentIntent_status" => "Invalid PaymentIntent status",
+    "code_not_match" => "Voucher Code not match",
+];
